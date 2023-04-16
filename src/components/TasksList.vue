0c3457f9937346de3aa4788ae945e41b12ed5cd7
kenedy kipkirui <kipkiruikenedy@gmail.com>
2023-04-16 11:30:49 +0300

    load more button

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -115,26 +115,28 @@
   <div
     class="min-h-screen bg-slate-400 grid grid-cols-1 md:grid-cols-12 mx-0 md:mx-10 gap-0 md:gap-6"
   >
-    <div class="bg-green-200 col-span-1 md:col-span-4 ">
-    <div v-for="task in tasks" class="m-2 space-y-5 "  @click="router.push(`/task/${task.id}`)">
-
-
-      <TaskCard 
-       :key="task.id" 
-       :title="task.title" 
-      
-       :status="task.status" 
-       :amount="task.amount" 
-       :location="task.location" 
-       :date="task.date_on" 
-       :date_before="task.date_before" 
-       :offers="task.offers" 
-       :time="task.time" />
+  <div class="bg-green-200 col-span-1 md:col-span-4">
+    <div v-for="(task, index) in visibleTasks" :key="task.id" class="m-2 space-y-5" @click="goToTaskDetails(task.id)">
+      <TaskCard
+        :title="task.title"
+        :status="task.status"
+        :amount="task.amount"
+        :location="task.location"
+        :date="task.date_on"
+        :date_before="task.date_before"
+        :offers="task.offers"
+        :time="task.time"
+      />
     </div>
+    <div class="flex justify-center">
+      <button v-if="visibleTasks.length < tasks.length" @click="loadMore" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Load More
+      </button>
     </div>
+  </div>
 
     <!-- MORE DETAILS -->
-    <div class="bg-gray-200 col-span-8">
+    <div class="bg-gray-200 col-span-8 fixed right-0 top-50">
     <div class="flex space-x-4 p-3">
       <div class="w-3/5 bg-white">
       <!-- ROW1 -->
@@ -249,6 +251,21 @@ axios.get('https://server.airtaska.com/public/api/categories')
   });
 
 
+
+
+
+const visibleTasks = ref([]); // array of tasks to display initially
+const increment = 6; // how many tasks to load each time
+
+function goToTaskDetails(id) {
+  this.$router.push(`/task/${id}`);
+}
+
+function loadMore() {
+  const startIndex = visibleTasks.value.length;
+  const endIndex = startIndex + increment;
+  visibleTasks.value = visibleTasks.value.concat(tasks.slice(startIndex, endIndex));
+}
 </script>
 
 
